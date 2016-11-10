77ba79d7372d25d7b68f03d3540ea8a34c2d9ad4
lesliewy <leslie_wangyang@163.com>
2016-11-10 15:38:45 +0800

    change

--- Shortcut/lsgit.sh
+++ Shortcut/lsgit.sh
@@ -2,9 +2,11 @@
 # 用于列出所有git 的status.
 # 新增repo时，需要处理 repoNames paths ommitedDirs
 ################################################################################################################################
+# set -x
+set -e
 
 # BEGIN 颜色
-STYPE=1   # 取值 0 1 2 3 4 5 6 7
+STYLE=1   # 取值 0 1 2 3 4 5 6 7
 FG=32     # 取值 30 31 32 33 34 35 36 37
 BG=40     # 取值 40 41 42 43 44 45 46 47
 # END
@@ -39,9 +41,9 @@ for ((index=0; index < numOfRepos; index++));do
    name=${repoNames[$index]}
    path=${paths[$index]}
    ommit=${ommitedDirs[$name]}
-   cd $path
-   echo -e "\033[${STYLE};${FG};${BG}m"$name $path "\033[0m"
-   output=`git status -s`
+   cd "$path"
+   echo -e "\033[${STYLE};${FG};${BG}m""$name" "$path" "\033[0m"
+   output=$(git status -s)
    # 换行处理，git status 得到的是一行 这里将ommit作为最后一个分段传给awk, 如果ommit中包含了该目录，就跳过不处理.
    a="$output $ommit"
    echo $a | awk '{
