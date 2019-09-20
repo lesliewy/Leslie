fb649c5f2eb5345f5958d8d389f61769ff29e4d3
lesliewy <leslie_wangyang@163.com>
2019-09-20 22:36:45 +0800

    添加处理微信公众号shell脚本

---
+++ Linux/shell/OtherUtils/wechat/transWX.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# 解决微信文章无法保存图片的问题.
+# a, 公众号文章用浏览器打开，右击，存储;  此时并不会下载公众号里的图片;
+# b, 保存完后，执行该脚本;
+# c, 浏览器打开新生成的html，再次右击，存储; 就可以将公众号里的图片下载到本地了.
+
+
+IFS=$'\n'
+for file in `find $1 -depth 1 -name "*.html"`; do
+   suffix=${file:(-8)}
+   if [ $suffix = "old.html" ]; then
+      # rm $file
+      continue
+   fi
+   sed  -e 's/data-src/src/g' -e 's/crossorigin="anonymous"//g'  -i.old.html "$file"
+done
+
